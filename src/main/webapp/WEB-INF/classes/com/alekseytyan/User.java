5683b3d17cea118ce2e7799c1ab93ff413d6c5c7
AlexeyTyan <alekseytyan45@gmail.com>
2021-03-30 15:25:47 +0300

    Initial commit

---
+++ src/main/webapp/WEB-INF/classes/com/alekseytyan/User.java
@@ -0,0 +1,19 @@
+package com.alekseytyan;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.RequestScoped;
+
+@ManagedBean(name = "user", eager = true)
+@RequestScoped
+public class User {
+
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
